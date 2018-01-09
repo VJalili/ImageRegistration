096a14d7d26e903ca5797e8ccad14a9af3dacf1c
vahid <jalili.vahid@gmail.com>
2018-01-09 15:31:47 -0800

    Added xml parser.

--- image_registration/__init__.py
+++ image_registration/__init__.py
@@ -1,7 +1,8 @@
 """
 some description of what this package does.
 """
-
+import xml.etree.ElementTree as ET
+from xml.etree.ElementTree import ParseError
 
 class ImageRegistration(object):
     def __init__(self, config, nuclei_file):
@@ -24,7 +25,42 @@ class ImageRegistration(object):
 
         :return: void
         """
-        print "in xml parse"
+        self.regions = {}
+        try:
+            tree = ET.parse(config)
+            root = tree.getroot()
+            if root.tag != "Annotations":
+                raise ParseError("Invalid root element `{}`!".format(root.tag))
+            for annotation in root:
+                if annotation.tag != "Annotation":
+                    raise ParseError("Invalid child tag `{}`!".format(annotation.tag))
+                regions = annotation.findall("Regions")
+                if len(regions) != 1:
+                    raise ParseError("Expect exactly one `Regions` element per `Annotation` element, found `{}` "
+                                     "elements.".format(len(regions)))
+                for region in regions[0].findall("Region"):
+                    vertices = region.findall("Vertices")
+                    if len(vertices) != 1:
+                        raise ParseError("Expect exactly one `Vertices` element per `Region` element, found `{}` "
+                                         "elements.".format(len(vertices)))
+                    region_vertices = []
+                    for vertex in vertices[0]:
+                        for item in vertex.items():
+                            if item[0].lower() == "x":
+                                x = item[1]
+                            if item[0].lower() == "y":
+                                y = item[1]
+                            if item[0].lower() == "z":
+                                z = item[1]
+                        try:
+                            region_vertices.append(Vertex(x ,y, z))
+                        except NameError:
+                            raise ParseError("The vertex `{}` does not have X and/or Y coordinate(s)".format(vertex.items()))
+
+        except ImportError:
+            raise
+        except ParseError as e:
+            raise ParseError("Invalid configuration at `{}`: {} -- unable to continue.".format(config, e.message))
 
     def analyze(self, image_file, channel):
         """
