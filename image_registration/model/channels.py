9d2f7465492420409506c7dde0fbbf7e2ce28ca4
vahid <jalili.vahid@gmail.com>
2018-01-09 11:55:58 -0800

    Implemented `Channels` class.

---
+++ image_registration/model/channels.py
@@ -0,0 +1,14 @@
+"""
+a short description for channels ...
+
+"""
+
+
+class Channels(object):
+    gray = {'CD20', 'CD56', 'RORGT', 'P16', 'CD3'}
+    red = {'FOXP3', 'CK5', 'CK14','PDL1', 'PD-L1', 'PD1', 'MHC II', 'MHCII', 'DC-SIGN', 'CK19', 'F480'}
+    green = {'DC LAMP', 'DC-LAMP', 'CD83', 'CD11C', 'TIM3'}
+    blue = {'CD45', 'CD4', 'CD8', 'EOEMS', 'TBR2', 'TBET', 'T-BET', 'GRZB', 'GRANZYME B', 'GRANZYMEB',
+            'ICOS', 'IDO', 'IL10','KI67', 'PD1', 'EOMES', 'CK', 'BTK', 'NKP46', 'CD56','CSF1R', 'CD66B-',
+            'CD66B','CD163', 'CD3CD20NK','PANCK','SMA', 'CD32056', 'CD320NKP46', 'CD3-20-56', 'TRYPTASE',
+            'CD68','ASCF1R', 'CD207', 'CD206', 'B220', 'LY6G', 'GATA3', 'CD11B', 'TCF1TCF7'}
