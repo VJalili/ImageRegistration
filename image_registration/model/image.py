9803e3ae6894093f826e067cdf6326e055038f54
vahid <jalili.vahid@gmail.com>
2018-01-10 13:40:42 -0800

    Added image class.

---
+++ image_registration/model/image.py
@@ -0,0 +1,21 @@
+"""
+some brief description about this class.
+"""
+
+
+class Image(object):
+
+    def __init__(self, marker, regions, width, height, level):
+        """
+
+        :param marker:
+        :param regions:
+        :param width:
+        :param height:
+        :param level:
+        """
+        self.marker = marker
+        self.regions = regions
+        self.width = width
+        self.height = height
+        self.level = level
