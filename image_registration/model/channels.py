59bd3c4174ca1f87dcd64a850f1a4a0098eba372
vahid <jalili.vahid@gmail.com>
2018-01-10 10:02:48 -0800

    Updated channels and created one big dictionary.

--- image_registration/model/channels.py
+++ image_registration/model/channels.py
@@ -5,10 +5,24 @@ a short description for channels ...
 
 
 class Channels(object):
-    gray = {'CD20', 'CD56', 'RORGT', 'P16', 'CD3'}
-    red = {'FOXP3', 'CK5', 'CK14','PDL1', 'PD-L1', 'PD1', 'MHC II', 'MHCII', 'DC-SIGN', 'CK19', 'F480'}
-    green = {'DC LAMP', 'DC-LAMP', 'CD83', 'CD11C', 'TIM3'}
-    blue = {'CD45', 'CD4', 'CD8', 'EOEMS', 'TBR2', 'TBET', 'T-BET', 'GRZB', 'GRANZYME B', 'GRANZYMEB',
-            'ICOS', 'IDO', 'IL10','KI67', 'PD1', 'EOMES', 'CK', 'BTK', 'NKP46', 'CD56','CSF1R', 'CD66B-',
-            'CD66B','CD163', 'CD3CD20NK','PANCK','SMA', 'CD32056', 'CD320NKP46', 'CD3-20-56', 'TRYPTASE',
-            'CD68','ASCF1R', 'CD207', 'CD206', 'B220', 'LY6G', 'GATA3', 'CD11B', 'TCF1TCF7'}
+    channels = {
+        # Gray channel:
+        'CD20': "Gray", 'CD56': "Gray", 'RORGT': "Gray", 'P16': "Gray", 'CD3': "Gray",
+
+        # Red channel:
+        'FOXP3': "Red", 'CK5': "Red", 'CK14': "Red", 'PDL1': "Red", 'PD-L1': "Red",
+        'PD1': "Red", 'MHC II': "Red", 'MHCII': "Red", 'DC-SIGN': "Red", 'CK19': "Red", 'F480': "Red",
+
+        # Gree channel:
+        'DC LAMP': 'Green', 'DC-LAMP': 'Green', 'CD83': 'Green', 'CD11C': 'Green', 'TIM3': 'Green',
+
+        # Blue channel:
+        'CD45': 'Blue', 'CD4': 'Blue', 'CD8': 'Blue', 'EOEMS': 'Blue', 'TBR2': 'Blue',
+        'TBET': 'Blue', 'T-BET': 'Blue', 'GRZB': 'Blue', 'GRANZYME B': 'Blue', 'GRANZYMEB': 'Blue',
+        'ICOS': 'Blue', 'IDO': 'Blue', 'IL10': 'Blue','KI67': 'Blue', 'PD1': 'Blue', 'EOMES': 'Blue',
+        'CK': 'Blue', 'BTK': 'Blue', 'NKP46': 'Blue', 'CD56': 'Blue','CSF1R': 'Blue', 'CD66B-': 'Blue',
+        'CD66B': 'Blue','CD163': 'Blue', 'CD3CD20NK': 'Blue','PANCK': 'Blue','SMA': 'Blue', 'CD32056': 'Blue',
+        'CD320NKP46': 'Blue', 'CD3-20-56': 'Blue', 'TRYPTASE': 'Blue', 'CD68': 'Blue','ASCF1R': 'Blue',
+        'CD207': 'Blue', 'CD206': 'Blue', 'B220': 'Blue', 'LY6G': 'Blue', 'GATA3': 'Blue', 'CD11B': 'Blue',
+        'TCF1TCF7': 'Blue'
+    }
