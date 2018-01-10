1c31dd19ebd279120e4bfd893c181343b77be4f1
vahid <jalili.vahid@gmail.com>
2018-01-10 12:55:18 -0800

    Added a function to region class to determine the width and height from the list of vertices.

--- image_registration/model/region.py
+++ image_registration/model/region.py
@@ -1,6 +1,9 @@
 """
 TODO: a brief description of region
 """
+import sys
+from pixel import *
+from vertex import *
 
 
 class Region(object):
@@ -12,6 +15,38 @@ class Region(object):
         self.id = id
         self.region = None
         self.bounding_region = None
+        self.pixels = None
+
+    def _get_width_and_height(self, vertices):
+        # Determine the upper-level corner of the rectangle from the list of vertices.
+        ul = Vertex(x=sys.maxint, y=sys.maxint, z=sys.maxint)
+        for vertex in vertices:
+            if vertex.x < ul.x and vertex.y < ul.y:
+                ul.x = vertex.x
+                ul.y = vertex.y
+
+        # Determine the upper-right corner of the rectangle from the list of vertices.
+        ur = Vertex(x=0, y=sys.maxint, z=sys.maxint)
+        for vertex in vertices:
+            if vertex.x > ur.x and vertex.y < ur.y:
+                ur.x = vertex.x
+                ur.y = vertex.y
+
+        # Determine the lower-left corner of the rectangle from a the list of vertices.
+        ll = Vertex(x=sys.maxint, y=0, z=sys.maxint)
+        for vertex in vertices:
+            if vertex.x < ll.x and vertex.y > ll.y:
+                ll.x = vertex.x
+                ll.y = vertex.y
+
+        # Determine the lower-right corner of the rectangle from a the list of vertices.
+        lr = Vertex(x=0, y=0, z=sys.maxint)
+        for vertex in vertices:
+            if vertex.x > lr.x and vertex.y > lr.y:
+                lr.x = vertex.x
+                lr.y = vertex.y
+
+        return ur.x - ul.x, ll.y-ul.y
 
     def set_region(self, vertices):
         """
@@ -32,3 +67,7 @@ class Region(object):
         :return:
         """
         self.bounding_region = vertices
+        w, h = self._get_width_and_height(vertices)
+        self.pixels = [[Pixel(0, 0, 0) for x in range(w)] for y in range(h)]
+
+
